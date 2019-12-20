e2459e38639dbad77a7bc2f3719ffbcf2d3eb135
c0der.br1t <c0der.br1t@gmail.com>
2019-12-20 10:45:22 -0600

    Built basic CharacterList and CharacterCard. Basic router in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Route, Switch } from 'react-router-dom';
 import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage";
+import CharacterList from './components/CharacterList';
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Switch>
+        <Route path='/CharacterList'>
+          <CharacterList />
+        </Route>
+        <Route path='/'>
+          <WelcomePage />
+        </Route>
+      </Switch>
+      {/* <WelcomePage /> */}
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,17 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({character}) {
+  console.log(character);
+  return (
+    <div>
+      <div>
+        <img src={character.image} />
+      </div>
+      <div>
+        <h3>{character.name}</h3>
+        <h4>{character.status}</h4>
+        <h4>{character.species}</h4>
+      </div>
+    </div>
+  );
 }
